15531652b9ff1ae775786fc3228ba2c343e61d47
le-s <steven.an.le@gmail.com>
2018-11-21 17:19:32 -0800

    Create #initialize, and setter/getter methods for class Session

--- skeleton/lib/session.rb
+++ skeleton/lib/session.rb
@@ -4,16 +4,27 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @req = req
   end
 
   def [](key)
+    @req.cookies[key]
   end
 
   def []=(key, val)
+    @req.cookies[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    # @cookie = res.set_cookie('_rails_lite_app', {:path => '/', :value => @req.to_json})
+
+    if self['_rails_lite_app'].nil?
+      res.set_cookie('_rails_lite_app', {:path => '/', :value => @req.to_json})
+    else
+      new_cookie = self['_rails_lite_app']
+      res.set_cookie('_rails_lite_app', new_cookie)
+    end
   end
 end
