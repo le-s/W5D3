d3abbe2f93e7529db828c2dd25738937c9e195b6
le-s <steven.an.le@gmail.com>
2018-11-21 16:07:07 -0800

    Complete phase 1 through 3 for Rails Lite

---
+++ skeleton/bin/p01_basic_server.rb
@@ -0,0 +1,20 @@
+require 'rack'
+require 'byebug'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  # debugger
+  if req.path == '/i/love/app/academy'
+    res.write 'i/love/app/academy'
+  else
+    res.write 'Hello World!'
+  end
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
---
+++ skeleton/lib/controller_base.rb
@@ -0,0 +1,57 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require 'byebug'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url, status_code = 302)
+    raise 'Cannot render twice' if already_built_response?
+    @res.status = status_code
+    @res['location'] = url
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise 'Cannot render twice' if already_built_response?
+    @already_built_response = false
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    p path = File.dirname(__FILE__)
+    new_path = File.join('/views/', '#{template_name}.html.erb')
+    file_content = File.read(new_path)
+    erb_code = ERB.new(file_content).result(binding)
+    render_content(erb_code)
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
